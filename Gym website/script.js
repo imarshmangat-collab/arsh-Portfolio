e03437a0bec7cec801502989d2087b3f5ee13136
imarshmangat-collab <imarshmangat@gmail.com>
2026-09-05 17:52:57 +0530

    Create script.js

---
+++ Gym website/script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll(".plan button").forEach(button => {
+    button.addEventListener("click", () => {
+        alert("Thanks for your interest! Please contact us to join PowerFit Gym.");
+    });
+});
